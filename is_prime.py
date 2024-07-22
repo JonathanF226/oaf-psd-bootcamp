342f3383785e23e868a1a177e4f2bfd9014bca92
JonathanF226 <flores.jonathanqv@gmail.com>
2024-07-22 18:15:56 -0400

    added some fixes mentioned in the pull request

--- is_prime.py
+++ is_prime.py
@@ -1,9 +1,22 @@
-def is_prime(n):
-    if n is None or not isinstance(n, int):
+import math
+
+"""Checks if the number is prime.
+
+Checks if the given number is an integer. Then checks if the number is greater than one. Finally checks
+if the number is divisible by any integer up to its square root.
+
+Input
+num (integer)
+
+Output
+Bool: True if the number is prime, false otherwise. 
+"""
+def is_prime(num: int) -> bool:
+    if num is None or not isinstance(num, int):
         return False
-    if n <= 1:
+    if num <= 1:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
             return False
     return True

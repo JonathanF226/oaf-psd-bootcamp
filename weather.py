eac28b95f390b162e0a148928fae9d94c8f158b9
JonathanF226 <flores.jonathanqv@gmail.com>
2024-07-31 23:07:41 -0400

    Added proper indentation for docstring

--- weather.py
+++ weather.py
@@ -1,37 +1,40 @@
-"""This class is in charge of getting data from API.
-
-The API should probably be in a .env file for security reasons. If not we can pass the API key directly here.
-"""
 class WeatherFetcher:
-    """Can pass the API key here and URL"""
+    """This class is in charge of getting data from API.
+
+    The API should probably be in a .env file for security reasons. If not we can pass the API key directly here.
+    """
     def __init__(self) -> None:
+        """Can pass the API key here and URL"""
         pass
 
-    """This method will get the data from an API for a given city and return in a dictionary"""
     def fetch_weather_data(self, city: str) -> dict:
+        """This method will get the data from an API for a given city and return in a dictionary"""
         pass
 
-"""This class will get specific data from the dictionary and return it."""
 class WeatherData:
-    """Initialize with fetched data"""
+    """This class will get specific data from the dictionary and return it."""
+
     def __init__(self, weather_data: dict) -> None:
+        """Initialize with fetched data"""
         pass
 
-    """This method will get and return temperature from the dictionary"""
     def get_temperature(self) -> float:
+        """This method will get and return temperature from the dictionary"""
         pass
 
-    """This method will get and return humidity from the dictionary"""
     def get_humidity(self) -> float:
+        """This method will get and return humidity from the dictionary"""
         pass
 
-"""This class will be in charge of displaying data."""
 class WeatherDisplay:
-    """Initialize with temperature and humidity"""
+    """This class will be in charge of displaying data."""
+
     def __init__(self, temperature: float, humidity: float) -> None:
+        """Initialize with temperature and humidity"""
         pass
 
-    """Display weather data such as temperature and humidity"""
     def display_weather(self) -> None:
+        """Display weather data such as temperature and humidity"""
         pass
 
+    
